1bea539010d93e99dae50ca22d5a7b7b8b3fbcea
brucewave <longthanh.dev@gmail.com>
2025-04-07 21:21:28 +0700

    fix main

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import GetAllList from './components/ListToDo';
 import OperationToDo from './components/OperationToDo';
 
 function App() {
   return (
     <div className="App">
-      {/* <GetAllList/> */}
       <OperationToDo/>
     </div>
   );
